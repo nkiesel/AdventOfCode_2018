e60b25cc1d990f6137c4f2ec138c5b0751735f7a
Norbert Kiesel <nk@iname.com>
2025-02-16 00:12:13 -0800

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.internal.config.LanguageFeature
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    alias(libs.plugins.versions)
+    alias(libs.plugins.versions.filter)
+    alias(libs.plugins.versions.update)
+}
+
+version = "2018"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(platform(libs.junit.bom))
+    implementation(libs.junit.jupiter)
+    implementation(libs.kotlin.serialization)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    minHeapSize = "1g"
+    maxHeapSize = "30g"
+    testLogging.showStandardStreams = true
+    filter {
+        setIncludePatterns("*Test", "Day*")
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+    @Suppress("UnsafeCompilerArguments")
+    sourceSets.all {
+        languageSettings {
+            enableLanguageFeature(LanguageFeature.WhenGuards.name)
+            enableLanguageFeature(LanguageFeature.MultiDollarInterpolation.name)
+            enableLanguageFeature(LanguageFeature.BreakContinueInInlineLambdas.name)
+        }
+    }
+    compilerOptions {
+        suppressWarnings = true
+    }
+}
