d8db8eaf7f1425b296afada8c103d307419954f3
Norbert Kiesel <nk@iname.com>
2025-03-07 22:47:29 -0800

    upgrade junit and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,10 @@ repositories {
 
 dependencies {
     testImplementation(libs.kotest.assertions.core)
-    testImplementation(platform(libs.junit.bom))
     implementation(libs.junit.jupiter)
+    implementation(libs.junit.jupiter.api)
+    implementation(libs.junit.jupiter.engine)
+    implementation(libs.junit.platform.launcher)
     implementation(libs.kotlin.serialization)
 }
 
