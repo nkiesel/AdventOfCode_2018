361fd8689d3949a491b43068ef99cc6bf683b578
Norbert Kiesel <nk@iname.com>
2025-05-16 23:46:48 -0700

    upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.internal.config.LanguageFeature
 
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
     alias(libs.plugins.versions.update)
 }
 
